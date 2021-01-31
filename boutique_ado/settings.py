737ba4609e0e673c3ea7f8866bc62a80a6c63ce5
mark.j0hnst0n <friendorfaux6@gmail.com>
2021-01-31 19:23:13 +0000

    deployment with typo on bucket name fixed

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -182,7 +182,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-ci-mjohnston'
+    AWS_STORAGE_BUCKET_NAME = 'boutiqueado-ci-mjohnston'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
